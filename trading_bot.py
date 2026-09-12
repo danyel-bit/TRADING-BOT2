d0e46c6d24af47cfd9ef99b5ececc00c1f5b90df
danyel-bit <danyelig59@gmail.com>
2026-09-12 16:48:07 -0400

    Update trading_bot.py

--- trading_bot.py
+++ trading_bot.py
@@ -14,8 +14,8 @@ logs/. Read those logs over time to see what worked and refine the rules yoursel
 
 import os
 import json
-import datetime from zoneinfo
-import ZoneInfo
+import datetime 
+from zoneinfo import ZoneInfo
 import requests
 
 # ---------- CONFIG ----------
